f26dc22872cd6c0426d5ca36c588cb45edcb6b19
Tomas Hrcka <thrcka@redhat.com>
2015-10-09 10:18:18 +0200

    Placeholder reuse

--- klaus.js
+++ klaus.js
@@ -7,20 +7,24 @@
 
 var program = require('commander');
 var path = require('path');
-var project_dir = path.resolve(process.cwd());
 var config = require('./lib/config');
-var rpm = require('./lib/rpm');
-var git = require('./lib/git');
+var package_json = require('package-json');
 
 program
   .version('0.0.1')
-  .option('-t, --tag', 'Tag your release')
-  .option('-d, --deploy', 'Deploy your release')
+  .option('-c, --checkdeps', 'Tag your release')
+  .option('-g, --generate-spec', 'Deploy your release')
   .option('-b, --build', 'Build distribution specfile/package from your nodejs app')
-  .option('-i, --init', 'Initialize gabelstaplerfahrer config files')
+  .option('-d, --download', 'Only download latest sources')
   .parse(process.argv);
 
-if (program.tag) console.log('  - not there jet');
+if (program.checkdeps){
+    package_json('pageres', 'latest', function (err, json) {
+      console.log(json);
+      //=> { name: 'pageres', ... }
+    });
+}
+
 if (program.deploy) console.log('  - not there jet');
 if (program.build) {
     rpm.BuildSpec(rpm.GetPackageJson(project_dir));
