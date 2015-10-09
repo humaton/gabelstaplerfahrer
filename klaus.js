5b274652706d44edab21e1e0d88a1f1e712fa2d7
Tomas Hrcka <thrcka@redhat.com>
2015-10-09 10:43:57 +0200

    Search and check dependencies options

--- klaus.js
+++ klaus.js
@@ -9,21 +9,32 @@ var program = require('commander');
 var path = require('path');
 var config = require('./lib/config');
 var package_json = require('package-json');
+var npmKeyword = require('npm-keyword');
+
 
 program
   .version('0.0.1')
-  .option('-c, --checkdeps', 'Tag your release')
+  .option('-c, --check_deps [package name]', 'Check dependencies for package')
   .option('-g, --generate-spec', 'Deploy your release')
   .option('-b, --build', 'Build distribution specfile/package from your nodejs app')
+  .option('-s, --search [package name]', 'Search for package in npmjs')
   .option('-d, --download', 'Only download latest sources')
   .parse(process.argv);
 
-if (program.checkdeps){
-    package_json('pageres', 'latest', function (err, json) {
+if (program.check_deps){
+    package_json(program.check_deps, 'latest', function (err, json) {
       console.log(json);
       //=> { name: 'pageres', ... }
     });
 }
+
+if (program.search) {
+    npmKeyword(program.search, function (err, packages) {
+    console.log(packages);
+    //=> [{name: 'gulp-autoprefixer', description: '...'}, ...]
+});
+}
+
 
 if (program.deploy) console.log('  - not there jet');
 if (program.build) {
