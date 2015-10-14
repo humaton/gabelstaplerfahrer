661ecad07da37b6502c8b3356e602e8c35697e36
Tomas Hrcka <thrcka@redhat.com>
2015-10-14 09:28:02 +0200

    cleanup

--- klaus.js
+++ klaus.js
@@ -7,16 +7,9 @@
 
 var program = require('commander');
 var path = require('path');
-var config = require('./lib/config');
-var package_json = require('package-json');
+var packageJson = require('package-json');
 var npmKeyword = require('npm-keyword');
-
-function get_package_json(package_name) {
-  package_json(package_name, 'latest', function (err, json) {
-      //console.log(json)
-      return json
-    });
-}
+var pkgdb = require('./pkgdb');
 
 program
   .version('0.0.1')
@@ -28,25 +21,24 @@ program
   .parse(process.argv);
 
 if (program.check_deps){
-   get_package_json(program.check_deps);
+  packageJson(program.check_deps, 'latest').then(json => {
+    console.log(json["dependencies"]);
+  });
+  
 }
 
 if (program.search) {
     npmKeyword(program.search, function (err, packages) {
-    console.log(packages);
-    //=> [{name: 'gulp-autoprefixer', description: '...'}, ...]
-});
+      console.log(packages);
+    });
 }
 
-
-if (program.deploy) console.log('  - not there jet');
 if (program.build) {
     rpm.BuildSpec(rpm.GetPackageJson(project_dir));
 }
 
 if (program.download){
-    package_json(program.check_deps, 'latest', function (err, json) {
+    package_json(program.download, 'latest', function (err, json) {
       console.log(json);
-      //=> { name: 'pageres', ... }
     });
 }
