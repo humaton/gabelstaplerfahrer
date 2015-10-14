8f6351b902b5f5df66220821a5cfc6027a589f68
Tomas Hrcka <thrcka@redhat.com>
2015-10-14 14:28:42 +0200

    coffe script transition

--- klaus.js
+++ klaus.js
@@ -1,44 +1,55 @@
-#!/usr/bin/env node
+###*
+#*  Module dependencies.
+#
+###
 
-/**
- **  Module dependencies.
- **/
+program = require('commander')
+path = require('path')
+packageJson = require('package-json')
+npmKeyword = require('npm-keyword')
+pkgdb = require('./pkgdb')
+util = require('util')
+gitlsremote = require('git-ls-remote')
+Sync = require('node-sync');
+request = require('request');
+
+rePattern = new RegExp(/^^~=/);
 
 
-var program = require('commander');
-var path = require('path');
-var packageJson = require('package-json');
-var npmKeyword = require('npm-keyword');
-var pkgdb = require('./pkgdb');
-
+createPackageData = (package_name) ->
+  return true
+  
 program
   .version('0.0.1')
   .option('-c, --check_deps [package name]', 'Check dependencies for package')
   .option('-g, --generate-spec', 'Deploy your release')
   .option('-b, --build', 'Build distribution specfile/package from your nodejs app')
   .option('-s, --search [package name]', 'Search for package in npmjs')
-  .option('-d, --download', 'Only download latest sources')
-  .parse(process.argv);
+  .option('-d, --download', 'Only download latest sources').parse process.argv
 
-if (program.check_deps){
-  packageJson(program.check_deps, 'latest').then(json => {
-    console.log(json["dependencies"]);
-  });
-  
-}
+if program.check_deps
+  packageJson program.check_deps, 'latest', (err, json) ->
+    #console.log(execSync("git ls-remote http://pkgs.fedoraproject.org/cgit/nodejs.git/"));
+    #execSync("git ls-remote http://pkgs.fedoraproject.org/cgit/nodejs.git/", puts);
+    #koji -q latest-build %s %s" % (tag, self.pkg_name)
+    #gitlsremote.head('http://pkgs.fedoraproject.org/cgit/nodejs-' + attributename + '.git')
+    deps = [] 
+    for attributename of json['dependencies']
+      #deps.push {name: attributename, version: json['dependencies'][attributename], distgitbranches: getDistGitBranches attributename }
+      console.log 'package: ' + attributename + ' version: ' + json['dependencies'][attributename] + ' is in fedora:' + pkgdb attributename
+    #console.log deps  
+    return
 
-if (program.search) {
-    npmKeyword(program.search, function (err, packages) {
-      console.log(packages);
-    });
-}
+if program.search
+  npmKeyword program.search, (err, packages) ->
+    console.log packages
+    return
 
-if (program.build) {
-    rpm.BuildSpec(rpm.GetPackageJson(project_dir));
-}
-
-if (program.download){
-    package_json(program.download, 'latest', function (err, json) {
-      console.log(json);
-    });
-}
+if program.build
+  #rpm.BuildSpec rpm.GetPackageJson(project_dir)
+  package_name = 'gyp'
+  console.log pkgdb package_name
+if program.download
+  package_json program.download, 'latest', (err, json) ->
+    console.log json
+    return
--- pkgdb.js
+++ pkgdb.js
@@ -1,10 +1,24 @@
+request = require("request")
+
 module.exports = function pkgdb(package_name, alias){
-	
+
 	return {
 		name: package_name,
-		type: 'folder',
-		path: alias,
-		items: walk(dir, alias)
+		version: 'folder',
+		distgitbranches: alias,
+		items: getDistGitBranches(package_name)
 	};
 
+};
+
+getDistGitBranches = function(package_name) {
+  return request('https://admin.fedoraproject.org/pkgdb/api/package/?pkgname=nodejs-' + package_name, function(error, response, body) {
+    var attributename, requestresult, result;
+    requestresult = JSON.parse(body);
+    result = [];
+    for (attributename in requestresult['packages']) {
+      result.push(requestresult['packages'][attributename]['collection']['branchname']);
+    }
+    return result;
+  });
 };
