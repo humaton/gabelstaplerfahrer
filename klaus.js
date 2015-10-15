f6e2ca4d0e4822c45518a5d13ff05d0528bacaf5
Tomas Hrcka <thrcka@redhat.com>
2015-10-15 16:39:10 +0200

    Check deps now work with pkgdb

--- klaus.js
+++ klaus.js
@@ -12,7 +12,7 @@ util = require('util')
 gitlsremote = require('git-ls-remote')
 colors = require('colors')
 exec = require('child_process').exec
-
+shell = require('shelljs')
 rePattern = new RegExp(/^^~=/);
 
 
@@ -28,19 +28,21 @@ program
   .option('-d, --download', 'Only download latest sources').parse process.argv
 
 if program.check_deps
+  
   packageJson program.check_deps, 'latest', (err, json) ->
-    #console.log(exec("git ls-remote http://pkgs.fedoraproject.org/cgit/nodejs.git/"));
-    #execSync("git ls-remote http://pkgs.fedoraproject.org/cgit/nodejs.git/", puts);
-    #koji -q latest-build %s %s" % (tag, self.pkg_name)
-    #gitlsremote.head('http://pkgs.fedoraproject.org/cgit/nodejs-' + attributename + '.git')
     deps = [] 
     for attributename of json['dependencies']
       deps.push {name: attributename, version: json['dependencies'][attributename], distgitbranches: getDistGitBranches attributename }
-      console.log 'package: ' + attributename.red + ' version: ' + json['dependencies'][attributename].green + ' is in fedora:' + pkgdb attributename, json
-      
-    #console.log deps
+    
     deps.forEach (dep) ->
-      console.log dep
+      dep['distgitbranches'].forEach (tag) ->
+        #result = exec('koji -q latest-build '+tag+' '+dep.name)
+	# Run external tool synchronously
+	#if (exec 'koji -q latest-build '+tag+' '+dep.name).code != 0
+	#  echo 'Error: Git commit failed'
+	#  exit 1
+        #console.log result
+      console.log 'package: ' + dep.name.red + ' version: ' + dep.version.green + ' is in fedora: ' + dep.distgitbranches
       
 if program.search
   npmKeyword program.search, (err, packages) ->
--- pkgdb.js
+++ pkgdb.js
@@ -6,12 +6,25 @@ module.exports = function pkgdb(package_name, package_json){
 
 getDistGitBranches = function(package_name) {
   result = [];
-  res = request('GET', 'https://admin.fedoraproject.org/pkgdb/api/package/?pkgname=nodejs-'+ package_name)
-  requestresult = JSON.parse(res.getBody());
-  for (attributename in requestresult['packages']) {
+  var requestresult = getPackageInfo(package_name);
+  
+  if (requestresult['packages']){
+    for (var attributename in requestresult['packages']) {
       result.push(requestresult['packages'][attributename]['collection']['branchname']);
-    }
+    } 
+  }else{
+    result.push('NONE')
+  }
   return result;  
 };
 
 getKojiBulds = function(package_name) {};
+getPackageInfo = function(package_name) {
+  try {
+      res = request('GET', 'https://admin.fedoraproject.org/pkgdb/api/package/?pkgname=nodejs-'+ package_name)
+      return requestresult = JSON.parse(res.getBody());
+  } catch (err) {
+    return false
+  }
+  
+};
