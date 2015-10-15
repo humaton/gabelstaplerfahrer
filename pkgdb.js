eee417822c6278b2efb94582f9816869cfbf8853
Tomas Hrcka <thrcka@redhat.com>
2015-10-15 10:46:11 +0200

    Use sync request

--- pkgdb.js
+++ pkgdb.js
@@ -1,24 +1,17 @@
-request = require("request")
+request = require('sync-request')
 
-module.exports = function pkgdb(package_name, alias){
-
-	return {
-		name: package_name,
-		version: 'folder',
-		distgitbranches: alias,
-		items: getDistGitBranches(package_name)
-	};
-
+module.exports = function pkgdb(package_name, package_json){
+	return getDistGitBranches(package_name)
 };
 
 getDistGitBranches = function(package_name) {
-  return request('https://admin.fedoraproject.org/pkgdb/api/package/?pkgname=nodejs-' + package_name, function(error, response, body) {
-    var attributename, requestresult, result;
-    requestresult = JSON.parse(body);
-    result = [];
-    for (attributename in requestresult['packages']) {
+  result = [];
+  res = request('GET', 'https://admin.fedoraproject.org/pkgdb/api/package/?pkgname=nodejs-'+ package_name)
+  requestresult = JSON.parse(res.getBody());
+  for (attributename in requestresult['packages']) {
       result.push(requestresult['packages'][attributename]['collection']['branchname']);
     }
-    return result;
-  });
+  return result;  
 };
+
+getKojiBulds = function(package_name) {};
